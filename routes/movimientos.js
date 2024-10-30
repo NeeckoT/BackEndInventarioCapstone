e1c487b76ba8e0e839c0592cd141c8560d7dcb9b
Nicolas Torres <nicolas.torres.aquevedo@gmail.com>
2024-10-29 23:04:57 -0300

    actualizacion 291024

--- routes/movimientos.js
+++ routes/movimientos.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../db');
 
-// Obtener todos los usuarios
 router.get('/', (req, res) => {
     connection.query('SELECT * FROM tbl_movimiento', (err, results) => {
         if (err) return res.status(500).send(err);
@@ -10,8 +9,36 @@ router.get('/', (req, res) => {
     });
 });
 
-// Crear un nuevo usuario
+router.get('/unitario', (req, res) => {
+    const id = req.query.id;
+
+    // Validación básica
+    if (!id) {
+        return res.status(400).json({ error: 'Se requiere un ID' });
+    }
+
+    connection.query(`
+        SELECT * FROM tbl_movimiento 
+        inner join tbl_medicamento tme ON tbl_medicamento_idmedicamento = idmedicamento
+        WHERE idmovimiento = ?;
+        `, 
+        [id],
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ error: 'Error en la base de datos' });
+            }
+            if (results.length === 0) {
+                return res.status(404).json({ error: 'Medicamento no encontrado' });
+            }
+
+            res.json(results[0]); // Devuelve solo el primer resultado
+        }
+    );
+});
+
 router.post('/', (req, res) => {
+
     const fechaActual = new Date();
 
     const dia = String(fechaActual.getDate()).padStart(2, '0');
@@ -80,5 +107,38 @@ router.post('/', (req, res) => {
     }
     
 });
-// Exporta el router
+
+
+router.post('/cajaInventario', (req, res) => {
+
+    console.log(req.body)
+    const data = req.body
+
+    let query = `INSERT INTO tbl_medicamento (
+        cantidadmovimiento,
+        fechavencimientomovimiento,
+        preciomovimiento,
+        preciounitariomovimiento,
+        fechamovimineto,
+        tipomovimiento,
+        activomovimiento,
+        tbl_medicamento_idmedicamento
+        )
+        VALUES `
+    data.forEach((movimiento)=>{
+        query += `(
+        ${movimiento.cantidad},
+        ${movimiento.fechavencimientomovimiento},
+        ${movimiento.preciomovimiento},
+        ${movimiento.fechamovimineto},
+        3,
+        1,
+        ${movimiento.tbl_medicamento_idmedicamento},
+        ),`
+        console.log(movimiento.idmedicamento);
+    })
+    console.log(query);
+
+});
+
 module.exports = router;
